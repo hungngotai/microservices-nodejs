067c00c312de5d7b8b84b8876f143d0d877e3492
Ngo Tai Hung <ngotaihung@gmail.com>
2023-03-18 22:17:31 +0700

    Complete client

--- comment_service/index.js
+++ comment_service/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { randomBytes } = require('crypto')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 4001
 const comments = {}
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/posts/:id/comments', (req, res) => {
--- post_service/index.js
+++ post_service/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { randomBytes } = require('crypto')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 4000
 const posts = {}
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/posts', (req, res) => res.status(200).send(posts))
@@ -12,7 +14,7 @@ app.get('/posts', (req, res) => res.status(200).send(posts))
 app.post('/posts', (req, res) => {
   const { title } = req.body
   const id = randomBytes(4).toString('hex')
-  posts[id] = { title }
+  posts[id] = { id, title }
   res.status(201).send(posts[id])
 })
 
